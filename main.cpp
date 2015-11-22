ae612498adeb10f2b2daff5809caafa778fd957f
jrv <jaap@tuxic.nl>
2015-11-22 18:43:34 +0100

    laser test w. delayed enable

--- main.cpp
+++ main.cpp
@@ -1,17 +1,34 @@
 #include "mbed.h"
 
-PwmOut pwm(p21);
-DigitalOut enable(p22);
+PwmOut pwm(p22);
+DigitalOut enable(p21);
 
-DigitalOut myled(LED2);
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+
+void setpower(int power);
+
 int main() {
+  led1=0;
+  led2=0;
   pwm.period(1.0/5000);
-  pwm.pulsewidth(1.0/50000);
-  enable = 1;
-    while(1) {
-        myled = 1;
-        wait(0.2);
-        myled = 0;
-        wait(0.2);
-    }
+  setpower(0);
+  while(1) {
+    enable = 0; led1 = 0;
+    wait(5);
+    setpower(0);
+    enable = 1; led1 = 1;
+    wait(5);
+    led2 = 1;
+    setpower(1);
+    wait(4);
+    led2 = 0;
+    setpower(10);
+    wait(1);
+  }
+}
+
+void setpower(int power) {
+	if ((power < 0) || (power > 100)) power = 0;
+	pwm.pulsewidth(power/500000.0);
 }
