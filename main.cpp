9a7a210684ebd3e8794ea09e4c0f4d8224db466c
Jaap Vermaas <jaap@tuxic.nl>
2016-01-20 12:30:04 +0100

    added button + potmeter power control

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "mbed.h"
 
+Serial pc(USBTX, USBRX); // tx, rx
 PwmOut pwm(p22);
 DigitalOut enable(p21);
+DigitalIn  button(p19);
+AnalogIn pwr(p18);
 
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
@@ -9,22 +12,24 @@ DigitalOut led2(LED2);
 void setpower(int power);
 
 int main() {
+  pc.printf("Starting\n\r");
   led1=0;
   led2=0;
+  enable = 0;
   pwm.period(1.0/5000);
   setpower(0);
   while(1) {
-    enable = 0; led1 = 0;
-    wait(5);
-    setpower(0);
-    enable = 1; led1 = 1;
-    wait(5);
-    led2 = 1;
-    setpower(1);
-    wait(4);
-    led2 = 0;
-    setpower(10);
+    int mpwr = (int)100*pwr;
+    pc.printf("Power: %d %\n\r", mpwr);
+    if (button) {
+	pc.printf("Start laser\n\r");
+	enable = led2 = led1 = 1;
+    	setpower(mpwr);
+    	while (button) wait(0.1);
+    	enable = led2 = 0;
+    }
     wait(1);
+    led1 = !led1;
   }
 }
 
