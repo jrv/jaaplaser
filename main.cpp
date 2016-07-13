06a97bcd26eafa03f078d863fa387bf78dfee96e
Jaap Vermaas <jaap@tuxic.nl>
2016-07-13 16:48:07 +0200

    last version july 2016

--- main.cpp
+++ main.cpp
@@ -8,32 +8,42 @@ AnalogIn pwr(p18);
 
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+DigitalOut laseron(p5);
 
-void setpower(int power);
+void setpower(float);
+
+#define PWMFREQ 20000	// pwm frequeny to use, est. 2..20 Khz
+#define TICKLE 0.0	// tickle value between 0..1 (where 1 = 100%)
 
 int main() {
+  laseron = 0;
+  led1=led2=led3=enable=0;
+  int cnt = 0;
   pc.printf("Starting\n\r");
-  led1=0;
-  led2=0;
-  enable = 0;
-  pwm.period(1.0/5000);
-  setpower(0);
+  pwm.period(1.0/PWMFREQ);
+  setpower(pwr);
   while(1) {
-    int mpwr = (int)100*pwr;
-    pc.printf("Power: %d %\n\r", mpwr);
     if (button) {
-	pc.printf("Start laser\n\r");
-	enable = led2 = led1 = 1;
-    	setpower(mpwr);
-    	while (button) wait(0.1);
-    	enable = led2 = 0;
+	setpower(pwr);
+	pc.printf("Fire laser\n\r");
+	enable = led3 = 1;
+    	while (button) wait(0.01);
+    	enable = led3 = 0;
+	cnt = 0;
     }
-    wait(1);
-    led1 = !led1;
+    wait(0.01);
+    if (cnt++ > 500) {
+	cnt = 0;
+    	led1 = !led1;
+    }
   }
 }
 
-void setpower(int power) {
-	if ((power < 0) || (power > 100)) power = 0;
-	pwm.pulsewidth(power/500000.0);
+void setpower(float power) {
+	if (power < TICKLE) power = TICKLE; // 0..80% duty cycle
+	if (power > 1) power = 1;
+	int perc = 100 * power;
+	pwm = power;
+	pc.printf("PWM %i", perc);
 }
